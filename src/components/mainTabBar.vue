0c59b0c738e3db9c2c68b4150ed730fda44f0981
小懒虫 <1987218701@qq.com>
2020-08-12 09:48:40 +0800

    初始化项目

---
+++ src/components/mainTabBar.vue
@@ -0,0 +1,41 @@
+<template>
+  <tab-bar>
+    <tab-bar-item path="/home">
+      <img slot="item-icon" src="~@/assets/img/tabbar/home.png" alt="">
+      <img slot="item-icon-active" src="~@/assets/img/tabbar/home-act.png" alt="">
+      <div slot="item-text">首页</div>
+    </tab-bar-item>
+    <tab-bar-item path="/classify">
+      <img slot="item-icon" src="~@/assets/img/tabbar/classify.png" alt="">
+      <img slot="item-icon-active" src="~@/assets/img/tabbar/classify-act.png" alt="">
+      <div slot="item-text">分类</div>
+    </tab-bar-item>
+    <tab-bar-item path="/shopcart">
+      <img slot="item-icon" src="~@/assets/img/tabbar/shopcar.png" alt="">
+      <img slot="item-icon-active" src="~@/assets/img/tabbar/shopcar-act.png" alt="">
+      <div slot="item-text">购物车</div>
+    </tab-bar-item>
+    <tab-bar-item path="/profile">
+      <img slot="item-icon" src="~@/assets/img/tabbar/profiles.png" alt="">
+      <img slot="item-icon-active" src="~@/assets/img/tabbar/profiles-act.png" alt="">
+      <div slot="item-text">我的</div>
+    </tab-bar-item>
+  </tab-bar>
+</template>
+
+<script>
+  import TabBar from "@/components/tabbar/TabBar";
+  import TabBarItem from "@/components/tabbar/TabBarItem";
+  
+  export default {
+    name: "mainTabBar",
+    components: {
+      TabBar,
+      TabBarItem
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/counter.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h2>{{$store.state.counter}}</h2>
+        <button>+</button>
+        <button>-</button>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: "counter"
+  }
+</script>
+
+<style scoped>
+
+</style>
